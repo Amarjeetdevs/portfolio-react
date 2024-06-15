ed6e5006a85b4814f11d596c950366005663ae42
Amarjeetdevs <129586496+Amarjeetdevs@users.noreply.github.com>
2024-06-15 16:19:56 +0530

    intial commit

---
+++ src/data/projects.js
@@ -0,0 +1,69 @@
+// Import images
+import WebImage1 from '../images/trident.jpeg';
+
+import WebImage3 from '../images/delhi.png';
+import WebImage4 from '../images/Milana.jpeg';
+
+import MobileImage1 from '../images/mobile-project-1.jpg';
+import MobileImage2 from '../images/mobile-project-2.jpg';
+import UIImage1 from '../images/ui-project-1.jpg';
+import UIImage2 from '../images/ui-project-2.jpg';
+
+export const projectsData = [
+	{
+		id: 1,
+		title: 'Healthcare',
+		category: 'Web Application',
+		img: 'https://raw.githubusercontent.com/Amarjeetdevs/Healthcare/d55887dd7e506da88ba38c7eaa3ec348ff9f3a74/client/src/assets/ss1.jpeg',
+		ProjectHeader: {
+			title: 'Project Management UI - From Context',
+			publishDate: 'May 26, 2024',
+			tags: 'UI / Full stck',
+		},
+	},
+	{
+		id: 2,
+		title: 'Trident Apparels',
+		category: 'Web Application',
+		img: WebImage1,
+	},
+	{
+		id: 3,
+		title: 'Delhi Restaurant and Chaat',
+		category: 'Web Application',
+		img: WebImage3,
+	},
+	{
+		id: 3,
+		title: 'Minala',
+		category: 'Web Site',
+		img: WebImage4,
+	},
+	{
+		id: 4,
+		title: 'Phoenix Digital Agency',
+		category: 'Mobile Application',
+		img: MobileImage2,
+	},
+	{
+		id: 4,
+		title: 'Project Management UI',
+		category: 'UI/UX Design',
+		img: UIImage1,
+	},
+	
+
+	{
+		id: 5,
+		title: 'React Social App',
+		category: 'Mobile Application',
+		img: MobileImage1,
+	},
+
+	{
+		id: 6,
+		title: 'Cloud Storage Platform',
+		category: 'UI/UX Design',
+		img: UIImage2,
+	},
+];
